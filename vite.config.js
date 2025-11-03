11840bcb7ddc79cfc5083902295777886cd8d058
dronoxonkaspa <dronoxonkaspa@gmail.com>
2025-11-02 23:59:16 -0800

    Fix: set output directory to dist for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   base: "./",
   plugins: [react()],
   build: {
-    outDir: path.resolve("C:/Users/israe/Projects/havenox-v2/frontend_output"),
-    emptyOutDir: true,
-    write: true
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
