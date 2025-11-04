e7caac1a34c6670ebcc24bd3a3f7dda56444c204
dronoxonkaspa <dronoxonkaspa@gmail.com>
2025-11-03 20:49:17 -0800

    Fix: use absolute import path for havenox.css

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../../../styles/havenox.css";
+import "/src/styles/havenox.css";
 
 export default function Footer() {
   return (
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../../../../styles/havenox.css";
+import "/src/styles/havenox.css";
 
 export default function Navbar() {
   return (
